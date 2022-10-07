6277836586da33a4c17a886e562e6e268093d958
ANSHxTHAKUR <sengaransh094@gmail.com>
2022-10-07 11:11:07 +0530

    chat app upload

---
+++ reactfirebasechatapp-master/src/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCx01caZq09F5-TSF72I8AWx2dTt26slxI",
+  authDomain: "real-chat-app-337f3.firebaseapp.com",
+  projectId: "real-chat-app-337f3",
+  storageBucket: "real-chat-app-337f3.appspot.com",
+  messagingSenderId: "134809748482",
+  appId: "1:134809748482:web:3b9d8aef355e517939931b"
+};
+
+
+
+export const app = initializeApp(firebaseConfig);
